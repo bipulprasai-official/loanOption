c36ae26f69debe6fb534d62986a7b58d944dddaf
bipul prasai <bipulprasai2020@gmail.com>
2022-11-17 00:50:39 +1100

    add load function when load button is clicked.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Button, Container, Row } from "react-bootstrap";
 import DataTable from "./components/DataTable";
@@ -6,6 +6,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { LoadAllUnis } from "./redux/actions/UniActions";
 
 function App() {
+  const [show, setShow] = useState(false);
+
   const dispatch = useDispatch();
   const AllUnis = useSelector((state) => state.universities);
 
@@ -14,16 +16,41 @@ function App() {
     dispatch(LoadAllUnis());
   }, [dispatch]);
 
+  // show/hide data when Load button is clicked
+  const fetchTableonClick = () => {
+    setShow(!show);
+  };
+
   return (
     <Container>
       <Row>
-        <div className="mt-3 d-flex justify-content-around">
-          <Button variant="secondary">Load</Button>
-          <Button variant="primary">ADD</Button>
-          <Button variant="danger">Remove</Button>
-        </div>
+        {show ? (
+          // Buttons  LOAD || ADD || REMOVE
 
-        <DataTable universities={AllUnis} />
+          <div className="mt-3 d-flex justify-content-around">
+            <Button variant="secondary" onClick={fetchTableonClick}>
+              Hide
+            </Button>
+            <Button variant="primary">ADD</Button>
+            <Button variant="danger">Remove</Button>
+          </div>
+        ) : (
+          <div className="mt-4 d-flex justify-content-around align-item-center">
+            <Button variant="secondary" onClick={fetchTableonClick}>
+              Load
+            </Button>
+          </div>
+        )}
+
+        {show ? (
+
+          // DATA TABLE components
+          <DataTable universities={AllUnis} />
+        ) : (
+          <div className="mt-4 d-flex justify-content-around align-item-center">
+            <h1>Please click Load button to view Data table.</h1>
+          </div>
+        )}
       </Row>
     </Container>
   );
